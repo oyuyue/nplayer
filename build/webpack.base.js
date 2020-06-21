09151626ba64a3390b71f657c0e5003e9c4d514e
wopen <nepow@foxmail.com>
2020-06-21 23:43:50 +0800

    优化打包

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -1,21 +1,32 @@
 const path = require('path');
+const fs = require('fs')
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const webpack = require('webpack');
 const version = require('../lerna.json').version;
 
+const buildName = process.env.npm_config_name;
+const packagePath = path.resolve(__dirname, '../packages')
+
+let packages = [];
+const entry = {}
+
+if (buildName) {
+  if (!fs.existsSync(path.resolve(packagePath, buildName))) {
+    console.error(buildName + ' 不存在')
+    process.exit(1)
+  }
+  packages.push(buildName)
+} else {
+  packages = fs.readdirSync(packagePath)
+}
+
+packages.forEach(name => {
+  entry[name.split('-').map(x => x.charAt(0).toUpperCase() + x.slice(1)).join('')] = path.resolve(packagePath, name, 'src/ts/index.ts')
+})
+
 module.exports = {
-  entry: {
-    RPlayer: path.resolve(__dirname, '../packages/rplayer/src/ts/index.ts'),
-    RPlayerAds: path.resolve(
-      __dirname,
-      '../packages/rplayer-ads/src/ts/index.ts'
-    ),
-    RPlayerDanmaku: path.resolve(
-      __dirname,
-      '../packages/rplayer-danmaku/src/ts/index.ts'
-    ),
-  },
+  entry,
 
   resolve: {
     extensions: ['.ts', '.js', '.scss', '.json'],
@@ -23,12 +34,8 @@ module.exports = {
 
   output: {
     path: path.resolve(__dirname, '..', 'packages'),
-    filename: (a) => {
-      const name = a.chunk.name
-        .match(/([A-Z]+[a-z]+)/g)
-        .map((x) => x.toLowerCase())
-        .join('-');
-      return name + '/dist/index.js';
+    filename: ({ chunk: { id } }) => {
+      return packages[id] + '/dist/index.js';
     },
     library: '[name]',
     libraryTarget: 'umd',
