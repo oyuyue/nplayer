c34d5be4be1712651320023d58a8d73409015636
wopen <nepow@foxmail.com>
2021-05-03 18:37:46 +0800

    fix: control item init multiple times

--- packages/nplayer/src/ts/parts/control/items/index.ts
+++ packages/nplayer/src/ts/parts/control/items/index.ts
@@ -4,6 +4,7 @@ import {
 } from 'src/ts/utils';
 import { Tooltip } from 'src/ts/components/tooltip';
 import { Disposable } from 'src/ts/types';
+import { EVENT } from 'src/ts/constants';
 import { ControlItem } from '..';
 import { spacerControlItem } from './spacer';
 
@@ -12,20 +13,20 @@ export class ControlBar extends Component {
 
   private spacer = spacerControlItem();
 
-  constructor(container: HTMLElement, private player: Player, items?: (ControlItem | string)[], private isTop = false) {
+  constructor(container: HTMLElement, private player: Player, items?: (ControlItem | string)[], private position = 0) {
     super(container, '.control_bar');
-    if (isTop) addClass(this.element, 'control_bar-top');
+    if (position === 2) addClass(this.el, 'control_bar-top');
     if (items) {
       const frag = document.createDocumentFragment();
       items.forEach((item) => {
         item = this.initControlItem(item) as ControlItem;
         if (item) {
-          frag.appendChild(item.element);
+          frag.appendChild(item.el);
           this.prevItems.push(item);
         }
       });
       this.updateTooltipPos();
-      this.element.appendChild(frag);
+      this.el.appendChild(frag);
     }
   }
 
@@ -39,27 +40,27 @@ export class ControlBar extends Component {
   private initControlItem = (item: ControlItem | string): ControlItem | void => {
     item = this.getItem(item) as ControlItem;
     if (item) {
-      if (!item.element) item.element = $();
+      if (!item.el) item.el = $();
       if (item.mounted) {
         if (item.tooltip) {
           item.tooltip.resetPos();
-          if (this.isTop) item.tooltip.setBottom();
+          if (this.position === 2) item.tooltip.setBottom();
         }
-        if (item.update) item.update(this.isTop);
+        if (item.update) item.update(this.position);
         return;
       }
 
       let tooltip: Tooltip | undefined;
-      if (item.tip) tooltip = new Tooltip(item.element, item.tip);
+      if (item.tip) tooltip = new Tooltip(item.el, item.tip);
       if (item.init) {
-        if (item.init.length > 2 && !tooltip) tooltip = new Tooltip(item.element);
-        item.init(this.player, this.isTop, tooltip as Tooltip);
+        if (item.init.length > 2 && !tooltip) tooltip = new Tooltip(item.el);
+        item.init(this.player, this.position, tooltip as Tooltip);
       }
       if (item.dispose) addDisposable(this, item as Disposable);
       if (!tooltip) tooltip = item.tooltip;
       if (tooltip) {
         tooltip.resetPos();
-        if (this.isTop) tooltip.setBottom();
+        if (this.position === 2) tooltip.setBottom();
       }
 
       item.mounted = true;
@@ -67,12 +68,16 @@ export class ControlBar extends Component {
     }
   }
 
+  private onHideControlItem = (item: ControlItem) => {
+    if (item.hide) item.hide();
+  }
+
   updateTooltipPos() {
     const last = this.prevItems.length - 1;
     this.prevItems.forEach((item, i) => {
       if (item.tooltip) {
         item.tooltip.resetPos();
-        if (this.isTop) item.tooltip.setBottom();
+        if (this.position === 2) item.tooltip.setBottom();
         if (i === 0) {
           item.tooltip.setLeft();
         } else if (i === last) {
@@ -100,9 +105,13 @@ export class ControlBar extends Component {
         if (item) items.push(item);
       });
 
-      patch(this.prevItems, items, this.element, this.initControlItem);
+      patch(this.prevItems, items, this.el, {
+        mount: this.initControlItem,
+        unmount: this.onHideControlItem,
+      });
       this.prevItems = items;
       this.updateTooltipPos();
+      this.player.emit(EVENT.CONTROL_ITEM_UPDATE);
     }
   }
 }
