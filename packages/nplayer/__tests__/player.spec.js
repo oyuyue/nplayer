8d83c8ee510f2ef5a2316fc1f32a82a7507fc476
wopen <nepow@foxmail.com>
2021-05-08 00:23:19 +0800

    fix(test): default options

--- packages/nplayer/__tests__/player.spec.js
+++ packages/nplayer/__tests__/player.spec.js
@@ -4,22 +4,46 @@ describe('Player', () => {
   test('default options', () => {
     const player = new Player()
     const opts = player.opts;
-    expect(opts.clickPause).toBe(true);
+
     expect(opts.contextMenuToggle).toBe(true);
     expect(opts.shortcut).toBe(true);
-    expect(opts.dblclickFullscreen).toBe(true);
     expect(opts.openEdgeInIE).toBe(true);
     expect(opts.posterEnable).toBe(true);
     expect(opts.seekStep).toBe(10);
     expect(opts.volumeStep).toBe(0.1);
     expect(opts.volumeBarWidth).toBe(100);
-    expect(opts.controls).toEqual(['play', 'volume', 'time', 'spacer', 'airplay', 'settings', 'web-fullscreen', 'fullscreen']);
+    expect(opts.controls).toEqual([
+      ['play', 'volume', 'time', 'spacer', 'airplay', 'settings', 'web-fullscreen', 'fullscreen'],
+      ['progress'], undefined
+    ]);
+    expect(opts.bpControls).toEqual({
+      650: [
+        ['play', 'progress', 'time', 'web-fullscreen', 'fullscreen'],
+        [],
+        ['spacer', 'airplay', 'settings'],
+      ],
+    });
     expect(opts.settings).toEqual(['speed']);
     expect(opts.contextMenus).toEqual(['loop', 'pip', 'version']);
-    expect(opts.videoAttrs).toEqual({
+    expect(opts.videoProps).toEqual({
       crossorigin: 'anonymous',
       preload: 'auto',
       playsinline: 'true',
+    });
+
+    const player2 = new Player({ live: true })
+    const opts2 = player2.opts;
+
+    expect(opts2.controls).toEqual([
+      ['play', 'volume', 'time', 'spacer', 'airplay', 'settings', 'web-fullscreen', 'fullscreen'],
+      undefined, undefined
+    ]);
+    expect(opts2.bpControls).toEqual({
+      650: [
+        ['play', 'time', 'web-fullscreen', 'fullscreen'],
+        [],
+        ['spacer', 'airplay', 'settings'],
+      ],
     });
   })
 })
