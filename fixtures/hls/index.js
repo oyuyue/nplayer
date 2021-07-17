ab03ee8f6898aa64365da721869e8a39400e9cb8
wopen <nepow@foxmail.com>
2021-07-17 23:03:54 +0800

    hls

---
+++ fixtures/hls/index.js
@@ -0,0 +1,4 @@
+window.onload = function () {
+  const hls = new NPlayerHls
+  hls.load('https://test-streams.mux.dev/x36xhzz/url_0/193039199_mp4_h264_aac_hd_7.m3u8')
+}
---
+++ packages/nplayer-hls/src/index.ts
@@ -0,0 +1,23 @@
+import { parseMedia } from './m3u8';
+
+export default class Hls {
+  load(url: string): void {
+    fetch(url)
+      .then((res) => res.text())
+      .then((text) => {
+        const frags = parseMedia(text, url).frags;
+        const index = 0;
+        this.loadFrag(frags[index]);
+      });
+  }
+
+  loadFrag(frag: any) {
+    fetch(frag.url)
+      .then((res) => res.arrayBuffer())
+      .then((buf) => this.demux(new Uint8Array(buf)));
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  demux(data: Uint8Array) {
+  }
+}
