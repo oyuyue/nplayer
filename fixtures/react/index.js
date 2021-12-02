84d076f30c5c245a88aa66551a2976fcb7d59a55
guotingting <1531297152@qq.com>
2021-12-02 19:54:53 +0800

    feat: nplayer-react component support deal stream media

--- fixtures/react/index.js
+++ fixtures/react/index.js
@@ -1,10 +1,22 @@
 window.onload = function () {
-  const e = React.createElement
+  const e = React.createElement;
+  const url = 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8';
 
-  ReactDOM.render(e(NPlayerReact, { 
-    style: { width: '100%', height: '100%' }, 
-    className: 'asd', 
-    options: { src: 'https://v-cdn.zjol.com.cn/280443.mp4' } }), 
-    document.querySelector('#app')
-  )
+  const handleStreamLoad = (playerInstance) => {
+    const hls = new Hls();
+    hls.attachMedia(playerInstance.video);
+    hls.on(Hls.Events.MEDIA_ATTACHED, () => {
+      hls.loadSource(url);
+    });
+  };
+
+
+  ReactDOM.render(
+      e(NPlayerReact, {
+        style: { width: '100%', height: '100%' },
+        onStreamLoad: handleStreamLoad,
+        options: { src: url }
+      }),
+      document.querySelector('#app')
+  );
 }
