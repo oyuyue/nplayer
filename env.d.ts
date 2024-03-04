981fdba1459670005326e8235429cc7b4f0c9131
oyuyue <oyuyue@outlook.com>
2024-03-04 22:43:59 +0800

    init

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="vite/client" />
+
+declare const __VERSION__: string;
+declare const ResizeObserver: any;
--- src/global.d.ts
+++
@@ -1,2 +0,0 @@
-declare const __VERSION__: string;
-declare const ResizeObserver: any;
